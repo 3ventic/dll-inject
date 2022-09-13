9b6ed471d90860dac0a9848e0e037b07026c7c35
3v <ventic@3v.fi>
2022-09-13 15:34:02 +0300

    add module_root to binding config

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-if (process.platform !== 'win32') return;
-var injector = require('bindings')('injector');
+if (process.platform !== "win32") return;
+var injector = require("bindings")({
+	bindings: "injector.node",
+	module_root: __dirname,
+});
 module.exports = injector;
